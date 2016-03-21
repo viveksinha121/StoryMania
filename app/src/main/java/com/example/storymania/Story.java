868e1f08f4cbd96bdcccad9f96b0dd7b524ef9e7
Vivek Sinha <vievksinha.vicky121@gmail.com>
2016-03-21 20:48:03 +0530

    Strory object created

---
+++ app/src/main/java/com/example/storymania/Story.java
@@ -0,0 +1,90 @@
+package com.example.storymania;
+
+/**
+ * Created by nightstay on 21/03/16.
+ */
+public class Story {
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public boolean is_following() {
+        return is_following;
+    }
+
+    public void setIs_following(boolean is_following) {
+        this.is_following = is_following;
+    }
+
+    public long getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(long followers) {
+        this.followers = followers;
+    }
+
+    public long getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(long following) {
+        this.following = following;
+    }
+
+    public long getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(long createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    String about,id,username,image,url,handle;
+    boolean is_following;
+    long followers,following,createdOn;
+}
